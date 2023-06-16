f8ab46c04a463de1943915649ffb00b58e832898
Pastilhas <joao.campos1221@gmail.com>
2023-06-16 13:57:15 +0100

    Refactor Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,29 @@
+import { JSX } from "preact";
+
+export function HeaderLink(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+  return (
+    <>
+      <a
+        href={props.href}
+        class="p-3 rounded-lg hover:bg-gray-600 hover:text-gray-100 bg-gray-100 text-gray-600"
+      >
+        <h2>
+          {props.children}
+        </h2>
+      </a>
+    </>
+  );
+}
+
 export function Header() {
   return (
     <>
-      <div class="p-3 mx-auto my-10 max-w-screen-md flex rounded-lg shadow bg-gray-100">
-        <a
-          href="/"
-          class="p-3 mx-3 font-bold rounded-lg hover:bg-gray-600 hover:text-gray-100 bg-gray-100 text-gray-600"
-        >
-          <h2>
-            Home
-          </h2>
-        </a>
-        <a
-          href="/personal"
-          class="p-3 mx-3 font-bold rounded-lg hover:bg-gray-600 hover:text-gray-100 bg-gray-100 text-gray-600"
-        >
-          <h2>
-            Personal
-          </h2>
-        </a>
-        <a
-          href="/projects"
-          class="p-3 mx-3 font-bold rounded-lg hover:bg-gray-600 hover:text-gray-100 bg-gray-100 text-gray-600"
-        >
-          <h2>
-            Projects
-          </h2>
-        </a>
+      <div class="p-3 mx-auto my-10 max-w-screen-md rounded-lg shadow bg-gray-100">
+        <div class="flex space-x-5 font-bold">
+          <HeaderLink href="/">Home</HeaderLink>
+          <HeaderLink href="/personal">Personal</HeaderLink>
+          <HeaderLink href="/projects">Projects</HeaderLink>
+        </div>
       </div>
     </>
   );
